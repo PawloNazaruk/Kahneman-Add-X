922804d26d779dc9120adb04d5e8680a6aed8ea6
Pawlo <pawlonazaruk@gmail.com>
2021-04-04 23:56:04 +0200

    Prepared min and max edges for generating random number

--- cls/NumberGame.py
+++ cls/NumberGame.py
@@ -1,11 +1,17 @@
 class AddX:
 
     def __init__(self, length=4):
-        self.length = length
-
+        self._length = length
 
     def display_number(self):
-        number = 3333
+        number_min = 1 * 10**(self._length - 1)
+        number_max = int("".join(["9" for x in range(self._length)]))
+        number = number_max
         return str(number)
 
-
+    """
+        min_number = 1 * 10**self.length
+        max_number = 9 * 10**self.length
+        rng = range(min_number, max_number)
+        print(rng)
+    """
--- tests/tests.py
+++ tests/tests.py
@@ -13,9 +13,28 @@ class TestNumberGame(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_display_number_with_four_length(self):
+    def test_display_number_with_default_length(self):
         # given
-        number = "3333"
         # when
+        result = self.game.display_number()
         # then
-        assert_that(self.game.display_number()).is_equal_to(number)
+        assert_that(result).is_length(4)
+
+    def test_display_number_with_given_length(self):
+        # given
+        length = 2
+        self.game._length = length
+        # when
+        result = self.game.display_number()
+        # then
+        assert_that(result).is_length(length)
+
+    def test_display_possible_max_number_with_default_length(self):
+        # given
+        test_max_number = "9999"
+        # when
+        result = self.game.display_number()
+        # then
+        assert_that(result).is_equal_to(test_max_number)
+
+
